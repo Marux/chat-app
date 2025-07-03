a70c441c859e1d016d925b236bc02d68e9720707
Victor Trimpai <victor.trimpai1987@gmail.com>
2025-07-03 18:32:06 -0400

    vTrimpai: Se crea controlador para conectar al servidor, se configura cors.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['https://websocket-arena.netlify.app'],
+      methods: ['GET', 'POST'],
+      credentials: true,
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
